6fdacf1848b0147e1c6afa1c8447fc7d3749e223
leerlandais <leerlandais@gmail.com>
2024-07-10 19:53:26 +0100

    feeding time

--- controller/publicController.php
+++ controller/publicController.php
@@ -4,14 +4,8 @@ use model\Manager\ArticleManager;
 use model\Mapping\ArticleMapping;
 
 $test = new ArticleManager($db);
-$newTest = new ArticleMapping([
-    "article_id" => "1",
-    "article_title" => "Test Title",
-    "article_slug" => "test-title",
-    "article_text" => "Test Text",
-    'user_user_id' => "1",
-]);
+$newTest = new ArticleMapping([]);
 
 
 
-echo $twig->render('base.html.twig', ["article" => $newTest]);
+echo $twig->render('publicView/public.home.view.twig', ["article" => null]);
